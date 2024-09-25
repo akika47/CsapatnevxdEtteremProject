7b26b6a48c0aa8261dab3ff00d148cc1181665ac
dkrisztian2 <20.dobos.krisztian.attila@mechwart.com>
2024-09-25 13:01:13 +0200

    mukodik a shopping cart

--- Order.xaml.cs
+++ Order.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Linq;
 
 namespace EtteremProject
 {
@@ -20,6 +21,8 @@ namespace EtteremProject
     /// </summary>
     public partial class Order : Window
     {
+        
+        
         public Order()
         {
             InitializeComponent();
@@ -30,7 +33,7 @@ namespace EtteremProject
                 btnAdmin.Visibility = Visibility.Visible;
             }
         }
-
+        
         private void OpenLoginWindow(object sender, RoutedEventArgs e)
         {
             /*
@@ -51,21 +54,38 @@ namespace EtteremProject
             this.Close();
         }
 
+        Dictionary<string, int> DishQunatitites = new Dictionary<string, int>();
         private void IncreaseDecrease(object sender, RoutedEventArgs e)
         {
             var gomb = sender as Button;
-            var xd = gomb.Parent as StackPanel;
-            var label = xd.Children[1] as Label;
+            var stackpanel = gomb.Parent as StackPanel;
+            var label = stackpanel.Children[1] as Label;
             if (gomb.Content.ToString() == "+")
             {
                 label.Content = Convert.ToInt32(label.Content) + 1;
+                if (!DishQunatitites.ContainsKey(stackpanel.Name))
+                {
+                    DishQunatitites.Add(stackpanel.Name, 1);
+                }
+                else
+                {
+                    DishQunatitites[stackpanel.Name] += 1;
+                }
+
             }
             else if (gomb.Content.ToString() == "-" && label.Content.ToString() != "0")
             {
                 label.Content = Convert.ToInt32(label.Content) - 1;
+
+                DishQunatitites[stackpanel.Name] -= 1;
             }
 
-
+            lbCart.Items.Clear();
+            foreach (var item in DishQunatitites) {
+                if (item.Value != 0)
+                    lbCart.Items.Add($"{item.Key}\t \t {item.Value}");         
+            }
+      
 
         }
 
